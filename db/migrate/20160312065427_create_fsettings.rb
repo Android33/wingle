8bc482aeb0cc9043f30e68c9af315214989c697d
Zaeem <devzaeem.asif@gmail.com>
2016-03-21 21:57:15 +0500

    migration default value fixed

--- db/migrate/20160312065427_create_fsettings.rb
+++ db/migrate/20160312065427_create_fsettings.rb
@@ -2,12 +2,12 @@ class CreateFsettings < ActiveRecord::Migration
   def change
     create_table :fsettings do |t|
       t.integer :user_id
-      t.string  :show_me_of_gende_with_interest, :default => C::FSettings::SHOW_ME_OF_GENDER_WITH_INTEREST[:SHOW_ME_ALL]
-      t.string  :show_me_close_to, :default => C::FSettings::SHOW_ME_CLOSE_TO[:SHOW_ME_CLOSE_TO_HERE]
-      t.string  :show_me_of_age_min, :default => C::FSettings::SHOW_ME_OF_AGE[:SHOW_ME_OF_AGE_MIN_DEFAULT]
-      t.string  :show_me_of_age_max, :default => C::FSettings::SHOW_ME_OF_AGE[:SHOW_ME_OF_AGE_MAX_DEFAULT]
+      t.string  :show_me_of_gende_with_interest, :default => 'Everybody'
+      t.string  :show_me_close_to, :default => 'SHOW_ME_CLOSE_TO_HERE'
+      t.string  :show_me_of_age_min, :default => '15'
+      t.string  :show_me_of_age_max, :default => '60'
       t.string  :show_me_of_city
-      t.string  :show_me_of_ethnicity, :default => C::FSettings::SHOW_ME_OF_ETHNICITY[:ETHNICITY_ALL]
+      t.string  :show_me_of_ethnicity, :default => 'ALL'
 
       t.timestamps
     end
