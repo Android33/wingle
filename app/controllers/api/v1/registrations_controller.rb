e7d0e81c1af6fb09fb5358e837f7d5bd6cfc4901
Zaeem <devzaeem.asif@gmail.com>
2016-04-26 10:40:47 +0500

    sign up fixed

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -6,25 +6,35 @@ class Api::V1::RegistrationsController < ApplicationController
   def create
     email = params[:user_email]
     password = params[:user_password]
-    login_type = params[:login_type]
-    name = params[:name]
 
     newUser = User.new;
-    newUser.email = email;
-    newUser.name = name;
+
+    newUser.email = email
+
     newUser.last_sign_in_at = Time.now
-    newUser.password = password;
-    newUser.login_type = login_type;
-    token = newUser.authentication_token;
+    newUser.password = password
+
+
+    if params[:name].present?
+      newUser.name = params[:name]
+    else
+      newUser.name = ""
+    end
+
+    if params[:login_type].present?
+      newUser.login_type = params[:login_type]
+    end
+
+    token = newUser.authentication_token
 
     if newUser.save
       user = User.find_by_email(email)
       token = user.authentication_token
       update_latlong(user, params[:latitude], params[:longitude])
-      render json: {STATUS_MSG: "NO_USER_INFO", id: user.id,  STATUS_CODE: CREATED_STATUS_CODE, user_token: token, user_email: email, name: name}
+      render json: {STATUS_MSG: "NO_USER_INFO", id: user.id,  STATUS_CODE: CREATED_STATUS_CODE, user_token: token, user_email: email, name: user.name}
 
     else
-      render json: {STATUS_MSG: "NO_USER_INFO", STATUS_CODE: CONFLICT_STATUS_CODE, user_token: "nil", user_email: nil, name: name}
+      render json: {STATUS_MSG: "NO_USER_INFO", STATUS_CODE: CONFLICT_STATUS_CODE, user_token: "nil", user_email: nil, name: ""}
     end
 
   end
