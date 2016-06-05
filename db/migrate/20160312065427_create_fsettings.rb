f8484d9fad9d68be4d9bbd452c01699c0626d84a
iamgenius <im.geniusman@gmail.com>
2016-06-05 16:20:21 +0500

    changed age defaults

--- db/migrate/20160312065427_create_fsettings.rb
+++ db/migrate/20160312065427_create_fsettings.rb
@@ -4,8 +4,8 @@ class CreateFsettings < ActiveRecord::Migration
       t.integer :user_id
       t.string  :show_me_of_gende_with_interest, :default => 'Everybody'
       t.string  :show_me_close_to, :default => 'SHOW_ME_CLOSE_TO_HERE'
-      t.string  :show_me_of_age_min, :default => '15'
-      t.string  :show_me_of_age_max, :default => '60'
+      t.string  :show_me_of_age_min, :default => '18'
+      t.string  :show_me_of_age_max, :default => '45'
       t.string  :show_me_of_city
       t.string  :show_me_of_ethnicity, :default => 'ALL'
 
